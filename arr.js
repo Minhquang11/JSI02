aa02ac1228ead138d3d726b5f4699ac15528623a
Minhquang11 <156317286+Minhquang11@users.noreply.github.com>
2024-05-11 17:56:13 +0700

    finish

--- arr.js
+++ arr.js
@@ -1,8 +1,28 @@
 let arr1 = [0,2,4,6,8];
 let arr2 = [1,3,5,7]
 
-for(let i of arr1){
-    if(i>3){
-        console.log(i)
-    }
-}
+
+// Cau a
+// for(let i of arr1){
+//     if(i>3){
+//         console.log(i)
+//     }
+// }
+
+// for(let i of arr2){
+//     if(i>3){
+//         console.log(i)
+//     }
+// }
+
+
+// Cau b
+// let listofarr = arr1.concat(arr2)
+// console.log(listofarr)
+
+
+// Cau c
+// let listofarr = arr1.concat(arr2)
+// for(let i=0; i <listofarr.length; i++){
+//     console.log(i)
+// }
