b6201da3a9b66600b77d33b02e64490a1346f393
Minhquang11 <156317286+Minhquang11@users.noreply.github.com>
2024-05-26 08:02:34 +0700

    add

---
+++ Day2/btvn.js
@@ -0,0 +1,32 @@
+//Cau 1
+// let arr1 = [1, 2, 3, 5];
+// let arr2 = [...arr1];
+
+// console.log(arr2);
+
+
+
+// let arr1 = [1, 2, 3, 5];
+// let arr2 = arr1.concat();
+
+// console.log(arr2);
+
+
+
+
+
+//Cau 2
+// let arrA = ["Hello", "Xin chào"];
+// let arrB = ["Bonjour", "Olá"];
+// let waysToSayHello = [...arrA, ...arrB];
+
+// console.log(waysToSayHello);
+
+
+
+
+// let arrA = ["Hello", "Xin chào"];
+// let arrB = ["Bonjour", "Olá"];
+// let waysToSayHello = arrA.concat(arrB);
+
+// console.log(waysToSayHello);
