b3a61f2ccfc1e1445df7364ac5692864a358b1d2
Minhquang11 <156317286+Minhquang11@users.noreply.github.com>
2024-06-18 09:59:58 +0700

    add

---
+++ BTVN/Day2/Day1/Day3/Day4/Day5/Day5.js
@@ -0,0 +1,56 @@
+let login = document.querySelector("#login")
+login.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let username = document.getElementById('username')
+    let email = document.getElementById('email')
+    let pw = document.getElementById("password")
+
+    let lowerCaseLetter = /[a-z]/g
+    let upperCaseLetter = /[A-Z]/g
+    let numbers = /[0-9]/g
+
+    if (email.value.trim().length == 0 && pw.value.trim().length == 0) {
+        alert("Please input your email & password")
+    }
+    else if (email.value.trim().length == 0) {
+        alert("Please input your email")
+    }
+    else if (pw.value.trim().length == 0) {
+        alert("Please input your password")
+    }
+    else if (pw.value.trim().length < 8) {
+        alert("Password must be at least 8 characters.")
+    }
+    else if (!pw.value.trim().match(lowerCaseLetter)) {
+        alert("Password must  contain a lowercase letter")
+    }
+    else if (!pw.value.trim().match(upperCaseLetter)) {
+        alert("Password must  contain a uppercase letter")
+    }
+    else if (!pw.value.trim().match(numbers)) {
+        alert("Password must  contain a number or special character")
+    }
+    else {
+        if (localStorage.users) {
+            let users = JSON.parse(localStorage.users)
+            users.push({
+                username: username.value.trim(),
+                email: email.value.trim(),
+                pw: pw.value.trim()
+            })
+            localStorage.setItem("users", JSON.stringify(users))
+        } else {
+            localStorage.setItem("users",
+                JSON.stringify(
+                    [
+                        {
+                            susername: username.value.trim(),
+                            email: email.value.trim(),
+                            pw: pw.value.trim()
+                        }
+                    ]));
+        }
+
+    }
+
+})
